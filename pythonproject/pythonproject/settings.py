57853daf78ae168d4212ed4e12bb30688e9927ab
emmanuel <emmacokonz@gmail.com>
2023-01-06 17:55:56 +0100

    fix custom login

--- pythonproject/pythonproject/settings.py
+++ pythonproject/pythonproject/settings.py
@@ -134,7 +134,7 @@ EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/accounts/login"
+LOGIN_URL = 'login'
 
 CART_SESSION_ID = 'cart'
 
